b2b7edc3b51e122a74ec686fc1e3f6d8d0271b66
asita26 <asita98890@gmail.com>
2023-11-23 22:52:40 +0530

    Comitting staged changes into master branch

--- LineComparisionProblem1.java
+++ LineComparisionProblem1.java
@@ -5,21 +5,7 @@ public class LineComparisonProblem1 {
         Scanner sc = new Scanner(System.in);
 
         // Input the coordinates of the two points for the line
-        System.out.print("Enter the x-coordinate of the first point: ");
-        double x1= sc.nextDouble();
-
-        System.out.print("Enter the y-coordinate of the first point: ");
-        double y1= sc.nextDouble();
-
-        System.out.print("Enter the x-coordinate of the second point: ");
-        double x2= sc.nextDouble();
-
-        System.out.print("Enter the y-coordinate of the second point: ");
-        double y2 = sc.nextDouble();
-        
-        double length=Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
-
-        System.out.print("Length of the line is:"+length);
+       
         
         
 
